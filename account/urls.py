0212895e156e66fc8998e73f9fe6df64110288f2
KemboiK <kipropkelvin374@gmail.com>
2025-04-08 09:10:59 +0300

    Update readme

--- account/urls.py
+++ account/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('', views.account_login, name="account_login"),
     path('register/', views.account_register, name="account_register"),
     path('logout/', views.account_logout, name="account_logout"),
+
+    # Password reset flow
+    path('forgot-password/', views.forgot_password, name='forgot_password'),
+    path('verify-otp/', views.verify_otp, name='verify_otp'),
+    path('reset-password/', views.reset_password, name='reset_password'),
 ]

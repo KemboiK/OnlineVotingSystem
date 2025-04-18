0b1ef829ac7e97b6c4148690f6997b41a99ff288
KemboiK <kipropkelvin374@gmail.com>
2025-04-18 22:47:13 +0300

    Update settings.py for deployment

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -184,3 +184,10 @@ EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Create from Google App Passwords
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://e-voting-jkuat-9feb3d297189.herokuapp.com',
+]

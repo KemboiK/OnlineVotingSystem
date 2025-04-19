1b4191cbba402daa368cddd7f5b1d25aaef0a466
KemboiK <kipropkelvin374@gmail.com>
2025-04-19 19:48:31 +0300

    Update settings for production

--- voting/forms.py
+++ voting/forms.py
@@ -5,29 +5,31 @@ from django.core.exceptions import ValidationError
 import re
 
 
-class VoterForm(FormSettings):
+class VoterForm(forms.ModelForm):
     class Meta:
         model = Voter
         fields = ['phone']
 
     def clean_phone(self):
-        phone = self.cleaned_data.get('phone', '').strip().replace(" ", "").replace("-", "")
+        phone = self.cleaned_data.get('phone')
 
+        # Remove any spaces
+        phone = phone.replace(" ", "")
+
+        # Rule 1: +254 format
         if phone.startswith('+254'):
-            phone = phone.replace('+254', '254', 1)
-        elif phone.startswith('07') and len(phone) == 10:
-            phone = '254' + phone[1:]
-        elif phone.startswith('254') and len(phone) == 12:
-            pass
-        else:
-            raise ValidationError("Phone number must start with +254 or 07 and be valid.")
+            if not re.fullmatch(r'\+2547\d{8}', phone):
+                raise forms.ValidationError("Phone must start with +254 ")
+        
+        # Rule 2: 07 format
+        elif phone.startswith('07'):
+            if not re.fullmatch(r'07\d{8}', phone):
+                raise forms.ValidationError("Phone must start with 07 and be exactly 10 digits long")
 
-        if not re.fullmatch(r'2547\d{8}', phone):
-            raise ValidationError("Enter a valid Kenyan number like 0712345678 or +254712345678")
+        else:
+            raise forms.ValidationError("Phone number must start with +254 or 07")
 
         return phone
-
-
 
 class PositionForm(FormSettings):
     class Meta:
